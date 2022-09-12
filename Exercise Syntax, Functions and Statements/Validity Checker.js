624ba9bc819a4a4ec609a95156d1f042edac37c7
gchulev <georgi.chulev@gmail.com>
2022-09-12 21:36:03 +0300

    Added SoftUni problem 'Validity Checker'

--- Exercise Syntax, Functions and Statements/Validity Checker.js
+++ Exercise Syntax, Functions and Statements/Validity Checker.js
@@ -1,8 +1,20 @@
-function validityChecker(valueX1, valueX2, valueY1, valueY2) {
-let x1 = valueX1;
-let x2 = valueX2;
-let y1 = valueY1;
-let y2 = valueY2;
+'use strict';
 
+function validityChecker(x1, y1, x2, y2) {
 
+    isValid(x1, y1, 0, 0);
+    isValid(x2, y2, 0, 0);
+    isValid(x1, y1, x2, y2);
 }
+
+function isValid(x1, y1, x2, y2){
+    let result = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+
+    if (Number.isInteger(result)) {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
+    }else{
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
+    }
+}
+
+validityChecker(2, 1, 1, 1);
