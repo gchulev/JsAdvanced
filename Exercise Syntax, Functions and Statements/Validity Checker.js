bcc88cc29d9d557b322ae0488b4d5de616d1b9ed
gchulev <georgi.chulev@gmail.com>
2022-09-13 11:36:44 +0300

    Fixed Validity Checker code

--- Exercise Syntax, Functions and Statements/Validity Checker.js
+++ Exercise Syntax, Functions and Statements/Validity Checker.js
@@ -5,16 +5,17 @@ function validityChecker(x1, y1, x2, y2) {
     isValid(x1, y1, 0, 0);
     isValid(x2, y2, 0, 0);
     isValid(x1, y1, x2, y2);
-}
-
-function isValid(x1, y1, x2, y2){
-    let result = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-
-    if (Number.isInteger(result)) {
-        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
-    }else{
-        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
+    
+    function isValid(x1, y1, x2, y2){
+        let result = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    
+        if (Number.isInteger(result)) {
+            console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
+        }else{
+            console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
+        }
     }
 }
+
 
 validityChecker(2, 1, 1, 1);
