6af5adb08335626eb6fd0573e4290f189611596b
gchulev <georgi.chulev@gmail.com>
2022-09-20 20:06:58 +0300

    Added SoftUni problem 'Orbit'

---
+++ Arrays and Nested Arrays/Orbit.js
@@ -0,0 +1,30 @@
+'use strict';
+
+function solve(input){
+    let rows = input[0];
+    let cols = input[1];
+    let starRow = input[2];
+    let starCol = input[3];
+
+    let matrix = [];
+
+    for (let i = 0; i < rows; i++) {
+        matrix[i] = [];
+    }
+
+    for ( let r = 0; r < rows; r++){
+        for (let c = 0; c < cols; c++) {
+            matrix[r][c] = Math.max(Math.abs(r - starRow) +1, Math.abs(c - starCol) + 1);            
+        }
+    }
+    
+    let result = '';
+    for (let i = 0; i < matrix.length; i++) {
+        result += matrix[i].join(' ') + '\n';
+    }
+    console.log(result.trimEnd());
+}
+
+// solve([4, 4, 0, 0]);
+solve([5, 5, 2, 2]);
+solve([3, 3, 2, 2]);
