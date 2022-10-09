dad626c63ab010c4e75e1df5aa8900bd51adfd0d
gchulev <georgi.chulev@gmail.com>
2022-10-09 09:21:40 +0300

    Added SoftUni problem 'Rectangle'

---
+++ Classes/1.Rectangle/rectangle.js
@@ -0,0 +1,19 @@
+'use strict';
+
+class Rectangle {
+    constructor(width, height, color) {
+        this.width = Number(width);
+        this.height = Number(height);
+        this.color = String(color);
+    }
+
+    calcArea() {
+        return this.width * this.height;
+    }
+}
+
+let rect = new Rectangle(4, 5, 'Red');
+console.log(rect.width);
+console.log(rect.height);
+console.log(rect.color);
+console.log(rect.calcArea());
